27cf6a99e81541bddfb6c34a95f7321d0717d449
zxzhao <zhengxiongzhao@gmail.com>
2025-07-12 15:18:51 +0800

    update

--- test-tcp.sh
+++ test-tcp.sh
@@ -5,11 +5,11 @@ PORT="9999"
 
 # if curl -v --connect-timeout 2 telnet://"$HOST":"$PORT" 2>&1 | grep -q "Connected"; then
 
-for i in $(seq 1 60); do
+for i in $(seq 1 120); do
   if nc -zv -w 1 $HOST $PORT >/dev/null 2>&1; then
-    echo "Port $PORT on $HOST is OPEN."
+    echo "$(date +'%Y-%m-%d %H:%M:%S.%3N') - Port $PORT on $HOST is OPEN."
   else
-    echo "Port $PORT on $HOST is CLOSED."
+    echo "$(date +'%Y-%m-%d %H:%M:%S.%3N') - Port $PORT on $HOST is CLOSED."
   fi
   sleep 1
 done
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-URL=http://127.0.0.1:8082/cong/admin/login.html
-for i in $(seq 1 60); do
-  curl -o /dev/null -s -w "%{http_code}\n" "$URL"
+URL=http://127.0.0.1:5001/health
+for i in $(seq 1 120); do
+  curl -o /dev/null -s -w "$(date +'%Y-%m-%d %H:%M:%S.%3N') - %{http_code}\n" "$URL"
   sleep 1 # 等待1秒
-donels
+done
